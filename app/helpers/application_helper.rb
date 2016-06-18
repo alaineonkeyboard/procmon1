69b45ee50be83160842fe9bcd495342b8f687064
Alaine A <alaine.uy@gmail.com>
2016-06-18 21:58:32 +0800

    more filtering fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
 	def sortable(column, title = nil)
 		title ||= column.titleize
-		css_class = column == sort_column ? "current #{sort_direction}" : nil
+		css_class = column == sort_column ? "current #{sort_direction} " : "current nosort "
 		direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-		link_to title, { sort: column, direction: direction, filter_column: filter_column, filter: filter_text }, {:class => css_class}
+		link_to title, { sort: column, direction: direction, filter_column: filter_column, 
+			filter: filter_text }, {:class => css_class}
 	end
 
 	def select_options
@@ -14,8 +15,6 @@ module ApplicationHelper
       { "text" => "Process Name", "value" => "name"},
       { "text" => "Process Owner", "value" => "user"}
     ]
-
-    puts options.class
 
 		select_tag :filter_column,
 			options_for_select(options.collect{ |i| [i["text"], i["value"]] })
